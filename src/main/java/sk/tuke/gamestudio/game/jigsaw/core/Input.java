d9f895b7e256393a288cb62312d31df0c60c850d
Šimon Trebišovský <simon.trebisovsky@student.tuke.sk>
2021-03-22 13:19:32 +0100

    .

--- src/main/java/sk/tuke/gamestudio/game/jigsaw/core/Input.java
+++ src/main/java/sk/tuke/gamestudio/game/jigsaw/core/Input.java
@@ -3,7 +3,7 @@ package sk.tuke.gamestudio.game.jigsaw.core;
 public class Input {
     private GameField gameField;
 
-    public Input(GameField gameField){
+    public Input(GameField gameField) {
         this.gameField = gameField;
     }
 
